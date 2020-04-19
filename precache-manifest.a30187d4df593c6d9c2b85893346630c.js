dd892b75136c8d1ff01f971ec08edb42e7ee763b
Asta Simke <simke.asta@gmail.com>
2020-04-19 21:34:23 +0300

    Updates

---
+++ precache-manifest.a30187d4df593c6d9c2b85893346630c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b8c7ba87ed79b9c5ae7ad215586f543",
+    "url": "/flickr-app/index.html"
+  },
+  {
+    "revision": "c8cd156b7a287f83f117",
+    "url": "/flickr-app/static/css/main.1922a702.chunk.css"
+  },
+  {
+    "revision": "7adc6259192c3e76bea9",
+    "url": "/flickr-app/static/js/2.65709888.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flickr-app/static/js/2.65709888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8cd156b7a287f83f117",
+    "url": "/flickr-app/static/js/main.2dcc377e.chunk.js"
+  },
+  {
+    "revision": "382168c46d259209d29f",
+    "url": "/flickr-app/static/js/runtime-main.8d789f51.js"
+  },
+  {
+    "revision": "6830ecbead3186c8b4a487fc98c2bf17",
+    "url": "/flickr-app/static/media/aperture-vintage-T-uuZBzB09M-unsplash.6830ecbe.jpg"
+  }
+]);
